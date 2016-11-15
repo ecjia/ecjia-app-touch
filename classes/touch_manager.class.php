5f9197dc6ea5cb82a00418f855c2cc602752d43b
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-15 15:33:10 +0800

    添加touch的API请求管理

---
+++ classes/touch_manager.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * ecjia touch manager API管理类
+ * @author royalwang
+ *
+ */
+class touch_manager extends Ecjia\System\Api\ApiManager
+{
+    
+    
+    /**
+     * 服务器地址
+     * @var serverHost
+     */
+    const serverHost = '/sites/api/?url=';
+     
+        
+    public function serverHost() {
+        return RC_Uri::home_url() . static::serverHost;
+    }
+    
+    
+}
+
+// end
