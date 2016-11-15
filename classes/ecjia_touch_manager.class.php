e2d2872a4bc4a4f4dc69ab780f70eb45f88c6bc4
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-15 18:42:39 +0800

    优化API管理类

--- classes/ecjia_touch_manager.class.php
+++ classes/ecjia_touch_manager.class.php
@@ -14,10 +14,20 @@ class ecjia_touch_manager extends Ecjia\System\Api\ApiManager
      */
     const serverHost = '/sites/api/?url=';
      
+    public function __construct() {
+        $this->header = array(
+        	'device_udid' => '',
+            'device_client' => 'html5',
+            'device_code' => '6004',
+            'api_version' => '1.2',
+        );
+    }
         
     public function serverHost() {
         return RC_Uri::home_url() . static::serverHost;
     }
+    
+    
     
     
 }
