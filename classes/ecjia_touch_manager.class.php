ffb91d6924eaf91bed2b21d4c08f7701d62337d6
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 15:50:29 +0800

    增加tab切换  	modified:   content/apps/orders/modules/order/detail_module.class.php  	modified:   content/apps/touch/classes/ecjia_touch_manager.class.php

--- classes/ecjia_touch_manager.class.php
+++ classes/ecjia_touch_manager.class.php
@@ -6,17 +6,17 @@
  */
 class ecjia_touch_manager extends Ecjia\System\Api\ApiManager
 {
-    
-    
+
+
     /**
      * 服务器地址
      * @var serverHost
      */
     const serverHost = '/sites/api/?url=';
-     
+
     public function __construct() {
         parent::__construct();
-        
+
         $this->header(array(
         	'device_udid'     => '',
             'device_client'   => 'html5',
@@ -24,15 +24,15 @@ class ecjia_touch_manager extends Ecjia\System\Api\ApiManager
             'api_version'     => '1.2',
         ));
     }
-        
+
     public function serverHost() {
-//         return RC_Uri::home_url() . static::serverHost;
-        return 'http://cityo2o.ecjia.com' . static::serverHost;
+        return RC_Uri::home_url() . static::serverHost;
+        // return 'http://cityo2o.ecjia.com' . static::serverHost;
     }
-    
-    
-    
-    
+
+
+
+
 }
 
 // end
