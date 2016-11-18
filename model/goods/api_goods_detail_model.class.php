ffacb2f6df26c1f8bf72ffbfeb38c2f1a97b84a7
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 14:30:46 +0800

    增加商品的API模型类

---
+++ model/goods/api_goods_detail_model.class.php
@@ -0,0 +1,13 @@
+<?php
+
+use Ecjia\System\Api\ApiModel;
+
+class api_goods_detail_model extends ApiModel
+{
+
+    public function __construct(array $input = array())
+    {
+
+    }
+
+}
---
+++ model/goods/api_goods_seller_list_model.class.php
@@ -0,0 +1,13 @@
+<?php
+
+use Ecjia\System\Api\ApiModel;
+
+class api_goods_seller_list_model extends ApiModel
+{
+
+    public function __construct(array $input = array())
+    {
+
+    }
+
+}
