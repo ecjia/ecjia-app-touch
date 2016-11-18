5e4978d2d4c3a008e600cf4fbbe7cf215ca82135
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-18 14:30:59 +0800

    为API模型类添加自动加载

--- functions/functions.func.php
+++ functions/functions.func.php
@@ -82,6 +82,115 @@ RC_Hook::add_action('class_ecjia_touch_manager',         function () {RC_Package
  * API模型加载
  * ========================================
  */
-RC_Hook::add_action('class_api_shop_config_model',         function () {RC_Package::package('app::touch')->loadModel('api_shop_config_model', false);});
+RC_Hook::add_action('class_api_shop_config_model',         function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_config_model', false);});
+RC_Hook::add_action('class_api_shop_help_detail_model',    function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_help_detail_model', false);});
+RC_Hook::add_action('class_api_shop_help_model',           function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_help_model', false);});
+RC_Hook::add_action('class_api_shop_info_detail_model',    function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_info_detail_model', false);});
+RC_Hook::add_action('class_api_shop_info_model',           function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_info_model', false);});
+RC_Hook::add_action('class_api_shop_payment_model',        function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_payment_model', false);});
+RC_Hook::add_action('class_api_shop_region_model',         function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_region_model', false);});
+RC_Hook::add_action('class_api_shop_token_model',          function () {RC_Package::package('app::touch')->loadModel('shop.api_shop_token_model', false);});
+
+
+RC_Hook::add_action('class_api_address_add_model',         function () {RC_Package::package('app::touch')->loadModel('address.api_address_add_model', false);});
+RC_Hook::add_action('class_api_address_delete_model',      function () {RC_Package::package('app::touch')->loadModel('address.api_address_delete_model', false);});
+RC_Hook::add_action('class_api_address_info_model',        function () {RC_Package::package('app::touch')->loadModel('address.api_address_info_model', false);});
+RC_Hook::add_action('class_api_address_list_model',        function () {RC_Package::package('app::touch')->loadModel('address.api_address_list_model', false);});
+RC_Hook::add_action('class_api_address_setDefault_model',  function () {RC_Package::package('app::touch')->loadModel('address.api_address_setDefault_model', false);});
+RC_Hook::add_action('class_api_address_update_model',      function () {RC_Package::package('app::touch')->loadModel('address.api_address_update_model', false);});
+
+
+RC_Hook::add_action('class_api_bonus_bind_model',          function () {RC_Package::package('app::touch')->loadModel('bonus.api_bonus_bind_model', false);});
+RC_Hook::add_action('class_api_bonus_coupon_model',        function () {RC_Package::package('app::touch')->loadModel('bonus.api_bonus_coupon_model', false);});
+RC_Hook::add_action('class_api_bonus_validate_model',      function () {RC_Package::package('app::touch')->loadModel('bonus.api_bonus_validate_model', false);});
+RC_Hook::add_action('class_api_receive_coupon_model',      function () {RC_Package::package('app::touch')->loadModel('bonus.api_receive_coupon_model', false);});
+RC_Hook::add_action('class_api_receive_coupon_model',      function () {RC_Package::package('app::touch')->loadModel('bonus.api_send_coupon_model', false);});
+
+
+RC_Hook::add_action('class_api_cart_create_model',         function () {RC_Package::package('app::touch')->loadModel('cart.api_cart_create_model', false);});
+RC_Hook::add_action('class_api_cart_delete_model',         function () {RC_Package::package('app::touch')->loadModel('cart.api_cart_delete_model', false);});
+RC_Hook::add_action('class_api_cart_list_model',           function () {RC_Package::package('app::touch')->loadModel('cart.api_cart_list_model', false);});
+RC_Hook::add_action('class_api_cart_update_model',         function () {RC_Package::package('app::touch')->loadModel('cart.api_cart_update_model', false);});
+RC_Hook::add_action('class_api_flow_checkOrder_model',     function () {RC_Package::package('app::touch')->loadModel('cart.api_flow_checkOrder_model', false);});
+RC_Hook::add_action('class_api_flow_checkOrder_model',     function () {RC_Package::package('app::touch')->loadModel('cart.api_flow_done_model', false);});
+
+
+RC_Hook::add_action('class_api_home_adsense_model',        function () {RC_Package::package('app::touch')->loadModel('home.api_home_adsense_model', false);});
+RC_Hook::add_action('class_api_home_category_model',       function () {RC_Package::package('app::touch')->loadModel('home.api_home_category_model', false);});
+RC_Hook::add_action('class_api_home_data_model',           function () {RC_Package::package('app::touch')->loadModel('home.api_home_data_model', false);});
+RC_Hook::add_action('class_api_home_discover_model',       function () {RC_Package::package('app::touch')->loadModel('home.api_home_discover_model', false);});
+RC_Hook::add_action('class_api_home_news_model',           function () {RC_Package::package('app::touch')->loadModel('home.api_home_news_model', false);});
+
+
+RC_Hook::add_action('class_api_merchant_goods_category_model',     function () {RC_Package::package('app::touch')->loadModel('merchant.api_merchant_goods_category_model', false);});
+RC_Hook::add_action('class_api_merchant_goods_list_model',         function () {RC_Package::package('app::touch')->loadModel('merchant.api_merchant_goods_list_model', false);});
+RC_Hook::add_action('class_api_merchant_goods_suggestlist_model',  function () {RC_Package::package('app::touch')->loadModel('merchant.api_merchant_goods_suggestlist_model', false);});
+RC_Hook::add_action('class_api_merchant_goods_suggestlist_model',  function () {RC_Package::package('app::touch')->loadModel('merchant.api_merchant_home_data_model', false);});
+
+
+
+RC_Hook::add_action('class_api_order_affirmReceived_model',        function () {RC_Package::package('app::touch')->loadModel('order.api_order_affirmReceived_model', false);});
+RC_Hook::add_action('class_api_order_cancel_model',                function () {RC_Package::package('app::touch')->loadModel('order.api_order_cancel_model', false);});
+RC_Hook::add_action('class_api_order_detail_model',                function () {RC_Package::package('app::touch')->loadModel('order.api_order_detail_model', false);});
+RC_Hook::add_action('class_api_order_express_model',               function () {RC_Package::package('app::touch')->loadModel('order.api_order_express_model', false);});
+RC_Hook::add_action('class_api_order_list_model',                  function () {RC_Package::package('app::touch')->loadModel('order.api_order_list_model', false);});
+RC_Hook::add_action('class_api_order_pay_model',                   function () {RC_Package::package('app::touch')->loadModel('order.api_order_pay_model', false);});
+RC_Hook::add_action('class_api_order_reminder_model',              function () {RC_Package::package('app::touch')->loadModel('order.api_order_reminder_model', false);});
+RC_Hook::add_action('class_api_order_update_model',                function () {RC_Package::package('app::touch')->loadModel('order.api_order_update_model', false);});
+
+
+RC_Hook::add_action('class_api_seller_category_model',             function () {RC_Package::package('app::touch')->loadModel('seller.api_seller_category_model', false);});
+RC_Hook::add_action('class_api_seller_collect_create_model',       function () {RC_Package::package('app::touch')->loadModel('seller.api_seller_collect_create_model', false);});
+RC_Hook::add_action('class_api_seller_collect_delete_model',       function () {RC_Package::package('app::touch')->loadModel('seller.api_seller_collect_delete_model', false);});
+RC_Hook::add_action('class_api_seller_collect_list_model',         function () {RC_Package::package('app::touch')->loadModel('seller.api_seller_collect_list_model', false);});
+RC_Hook::add_action('class_api_seller_list_model',                 function () {RC_Package::package('app::touch')->loadModel('seller.api_seller_list_model', false);});
+RC_Hook::add_action('class_api_seller_search_model',               function () {RC_Package::package('app::touch')->loadModel('seller.api_seller_search_model', false);});
+
+
+
+RC_Hook::add_action('class_api_connect_bind_model',                function () {RC_Package::package('app::touch')->loadModel('user.api_connect_bind_model', false);});
+RC_Hook::add_action('class_api_connect_signin_model',              function () {RC_Package::package('app::touch')->loadModel('user.api_connect_signin_model', false);});
+RC_Hook::add_action('class_api_connect_signup_model',              function () {RC_Package::package('app::touch')->loadModel('user.api_connect_signup_model', false);});
+
+RC_Hook::add_action('class_api_user_account_cancel_model',         function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_cancel_model', false);});
+RC_Hook::add_action('class_api_user_account_deposit_model',        function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_deposit_model', false);});
+RC_Hook::add_action('class_api_user_account_log_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_log_model', false);});
+RC_Hook::add_action('class_api_user_account_pay_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_pay_model', false);});
+RC_Hook::add_action('class_api_user_account_pay_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_raply_model', false);});
+RC_Hook::add_action('class_api_user_account_pay_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_record_model', false);});
+RC_Hook::add_action('class_api_user_account_update_model',         function () {RC_Package::package('app::touch')->loadModel('user.api_user_account_update_model', false);});
+
+RC_Hook::add_action('class_api_user_account_update_model',         function () {RC_Package::package('app::touch')->loadModel('user.api_user_collect_create_model', false);});
+RC_Hook::add_action('class_api_user_collect_delete_model',         function () {RC_Package::package('app::touch')->loadModel('user.api_user_collect_delete_model', false);});
+RC_Hook::add_action('class_api_user_collect_delete_model',         function () {RC_Package::package('app::touch')->loadModel('user.api_user_collect_list_model', false);});
+
+RC_Hook::add_action('class_api_user_collect_delete_model',         function () {RC_Package::package('app::touch')->loadModel('user.api_user_forget_password_model', false);});
+RC_Hook::add_action('class_api_user_info_model',                   function () {RC_Package::package('app::touch')->loadModel('user.api_user_info_model', false);});
+RC_Hook::add_action('class_api_user_password_model',               function () {RC_Package::package('app::touch')->loadModel('user.api_user_password_model', false);});
+RC_Hook::add_action('class_api_user_password_model',               function () {RC_Package::package('app::touch')->loadModel('user.api_user_reset_password_model', false);});
+RC_Hook::add_action('class_api_user_password_model',               function () {RC_Package::package('app::touch')->loadModel('user.api_user_send_pwdmail_model', false);});
+RC_Hook::add_action('class_api_user_password_model',               function () {RC_Package::package('app::touch')->loadModel('user.api_user_signin_model', false);});
+RC_Hook::add_action('class_api_user_password_model',               function () {RC_Package::package('app::touch')->loadModel('user.api_user_signout_model', false);});
+RC_Hook::add_action('class_api_user_signup_model',                 function () {RC_Package::package('app::touch')->loadModel('user.api_user_signup_model', false);});
+RC_Hook::add_action('class_api_user_signup_model',                 function () {RC_Package::package('app::touch')->loadModel('user.api_user_signupFields_model', false);});
+RC_Hook::add_action('class_api_user_snsbind_model',                function () {RC_Package::package('app::touch')->loadModel('user.api_user_snsbind_model', false);});
+RC_Hook::add_action('class_api_user_snsbind_model',                function () {RC_Package::package('app::touch')->loadModel('user.api_user_update_model', false);});
+
+RC_Hook::add_action('class_api_validate_account_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_validate_account_model', false);});
+RC_Hook::add_action('class_api_validate_account_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_validate_bind_model', false);});
+RC_Hook::add_action('class_api_validate_account_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_validate_bonus_model', false);});
+RC_Hook::add_action('class_api_validate_account_model',            function () {RC_Package::package('app::touch')->loadModel('user.api_validate_forget_password_model', false);});
+RC_Hook::add_action('class_api_validate_integral_model',           function () {RC_Package::package('app::touch')->loadModel('user.api_validate_integral_model', false);});
+RC_Hook::add_action('class_api_validate_integral_model',           function () {RC_Package::package('app::touch')->loadModel('user.api_validate_signin_model', false);});
+
+
+RC_Hook::add_action('class_api_goods_category_model',              function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_category_model', false);});
+RC_Hook::add_action('class_api_goods_category_model',              function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_comments_model', false);});
+RC_Hook::add_action('class_api_goods_seller_list_model',           function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_seller_list_model', false);});
+RC_Hook::add_action('class_api_goods_seller_list_model',           function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_suggestlist_model', false);});
+RC_Hook::add_action('class_api_goods_seller_list_model',           function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_detail_model', false);});
+RC_Hook::add_action('class_api_goods_seller_list_model',           function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_desc_model', false);});
+RC_Hook::add_action('class_api_goods_filter_model',                function () {RC_Package::package('app::touch')->loadModel('goods.api_goods_filter_model', false);});
+
 
 
