50647211c1fcbadb1262949537a8f57bb0d35c69
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-23 18:37:56 +0800

    增加h5站的user登录类封装

--- classes/ecjia_touch_user.class.php
+++ classes/ecjia_touch_user.class.php
@@ -3,6 +3,8 @@
 class ecjia_touch_user extends RC_Object
 {
     
+    const API_USER_COOKIE = 'api_token';
+    
     /**
      * 登录
      */
@@ -12,16 +14,51 @@ class ecjia_touch_user extends RC_Object
         	'name' => 'test100',
             'password' => 'a123456',
         );
-        $res = ecjia_touch_manager::make()->api(ecjia_touch_api::USER_SIGNIN)->data($data)->send();
-        _dump($res,1);
+        $api = ecjia_touch_manager::make()->api(ecjia_touch_api::USER_SIGNIN)->data($data);
+        $res = $api->run();
+        if ( ! $res) {
+            return $api->getError();
+        }
+        
+        $sid = array_get($res, 'session.sid');
+        
+        $minutes = RC_Config::get('cookie.lifetime');
+        $response = royalcms('response');
+        $response->withCookie(RC_Cookie::make(self::API_USER_COOKIE, $sid, $minutes));
+        
+        $this->cacheUserinfo($sid, array_get($res, 'user'));
+        
+        return array_get($res, 'user');
     }
+    
+    protected function cacheUserinfo($cookieid, $user) 
+    {
+        $cache_key = 'api_request_user_info::' . $cookieid;
+        
+        RC_Cache::app_cache_set($cache_key, $user, 'touch');
+    }
+    
+    protected function getCacheUserinfo()
+    {
+        $cache_key = 'api_request_user_info::' . RC_Cookie::get(self::API_USER_COOKIE);
+        
+        $data = RC_Cache::app_cache_get($cache_key, 'touch');
+
+        return $data ?: array();
+    }
+    
     
     /**
      * 检查是否登录
      */
     public function isSignin() 
     {
-        
+        $user = $this->getCacheUserinfo();
+        if (array_get($user, 'id') > 0 && array_get($user, 'name')) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
     /**
@@ -30,6 +67,12 @@ class ecjia_touch_user extends RC_Object
     public function signout()
     {
         
+    }
+    
+    
+    public function getUserinfo()
+    {
+        return $this->getCacheUserinfo();
     }
     
     
