a0677999f74becc6a103d2289f054cad8f4f76ce
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-26 14:58:23 +0800

    添加H5应用配置菜单

---
+++ classes/hooks/admin_touch.class.php
@@ -0,0 +1,21 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+
+class touch_admin_hooks {
+	
+   public static function append_admin_setting_group($menus) 
+   {
+       $setting = ecjia_admin_setting::singleton();
+       
+       $menus[] = ecjia_admin::make_admin_menu('divider', '', '', 10)->add_purview(array('mobile_config_manage', 'mobile_manage'));
+       $menus[] = ecjia_admin::make_admin_menu('wap', $setting->cfg_name_langs('wap'), RC_Uri::url('setting/shop_config/init', array('code' => 'wap')), 11)->add_purview('shop_config')->add_icon('fontello-icon-tablet');
+       
+       return $menus;
+   }
+    
+}
+
+RC_Hook::add_action( 'append_admin_setting_group', array('touch_admin_hooks', 'append_admin_setting_group') );
+
+
+// end
