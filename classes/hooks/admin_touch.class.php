4b63477f597b4beebd74cd1871e9a648e143f321
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-28 16:20:44 +0800

    更新touch应用的设置菜单名称

--- classes/hooks/admin_touch.class.php
+++ classes/hooks/admin_touch.class.php
@@ -7,7 +7,7 @@ class touch_admin_hooks {
    {
        $setting = ecjia_admin_setting::singleton();
        
-       $menus[] = ecjia_admin::make_admin_menu('divider', '', '', 10)->add_purview(array('mobile_config_manage', 'mobile_manage'));
+       $menus[] = ecjia_admin::make_admin_menu('nav-header', 'H5应用', '', 10)->add_purview(array('mobile_config_manage', 'mobile_manage'));
        $menus[] = ecjia_admin::make_admin_menu('wap', $setting->cfg_name_langs('wap'), RC_Uri::url('setting/shop_config/init', array('code' => 'wap')), 11)->add_purview('shop_config')->add_icon('fontello-icon-tablet');
        
        return $menus;
