27f06cfed52fd6f274922a795492909d432cfc14
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-11 10:50:00 +0800

    H5配置清理

--- classes/touch_installer.class.php
+++ classes/touch_installer.class.php
@@ -59,34 +59,34 @@ class touch_installer  extends ecjia_installer {
     }
 
     public function install() {
-        if (!ecjia::config(touch::STORAGEKEY_template, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('hidden', touch::STORAGEKEY_template, '', array('type' => 'hidden'));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_stylename, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('hidden', touch::STORAGEKEY_stylename, '', array('type' => 'hidden'));
-        }
+//         if (!ecjia::config(touch::STORAGEKEY_template, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('hidden', touch::STORAGEKEY_template, '', array('type' => 'hidden'));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_stylename, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('hidden', touch::STORAGEKEY_stylename, '', array('type' => 'hidden'));
+//         }
 
-        if (!ecjia::config(touch::STORAGEKEY_pc_url, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_pc_url, '', array('type' => 'text', 'sort_order' => 1));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_touch_url, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_touch_url, '', array('type' => 'text', 'sort_order' => 2));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_iphone_download, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_iphone_download, '', array('type' => 'text', 'sort_order' => 3));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_android_download, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_android_download, '', array('type' => 'text', 'sort_order' => 4));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_ipad_download, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_ipad_download, '', array('type' => 'text', 'sort_order' => 5));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_app_icon, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_app_icon, '', array('type' => 'file', 'store_dir' => 'data/', 'sort_order' => 6));
-        }
-        if (!ecjia::config(touch::STORAGEKEY_app_description, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_app_description, '', array('type' => 'text', 'sort_order' => 7));
-        }
+//         if (!ecjia::config(touch::STORAGEKEY_pc_url, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_pc_url, '', array('type' => 'text', 'sort_order' => 1));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_touch_url, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_touch_url, '', array('type' => 'text', 'sort_order' => 2));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_iphone_download, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_iphone_download, '', array('type' => 'text', 'sort_order' => 3));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_android_download, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_android_download, '', array('type' => 'text', 'sort_order' => 4));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_ipad_download, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_ipad_download, '', array('type' => 'text', 'sort_order' => 5));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_app_icon, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_app_icon, '', array('type' => 'file', 'store_dir' => 'data/', 'sort_order' => 6));
+//         }
+//         if (!ecjia::config(touch::STORAGEKEY_app_description, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_app_description, '', array('type' => 'text', 'sort_order' => 7));
+//         }
         if (!ecjia::config(touch::STORAGEKEY_map_qq_key, ecjia::CONFIG_CHECK)) {
         	ecjia_config::instance()->insert_config('wap', touch::STORAGEKEY_map_qq_key, '', array('type' => 'text', 'sort_order' => 8));
         }
@@ -96,33 +96,36 @@ class touch_installer  extends ecjia_installer {
 
     public function uninstall() {
 
-        if (ecjia::config(touch::STORAGEKEY_template, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_template);
-        }
-        if (ecjia::config(touch::STORAGEKEY_stylename, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_stylename);
-        }
+//         if (ecjia::config(touch::STORAGEKEY_template, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_template);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_stylename, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_stylename);
+//         }
 
-        if (ecjia::config(touch::STORAGEKEY_pc_url, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_pc_url);
-        }
-        if (ecjia::config(touch::STORAGEKEY_touch_url, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_touch_url);
-        }
-        if (ecjia::config(touch::STORAGEKEY_iphone_download, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_iphone_download);
-        }
-        if (ecjia::config(touch::STORAGEKEY_android_download, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_android_download);
-        }
-        if (ecjia::config(touch::STORAGEKEY_ipad_download, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_ipad_download);
-        }
-        if (ecjia::config(touch::STORAGEKEY_app_icon, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_app_icon);
-        }
-        if (ecjia::config(touch::STORAGEKEY_app_description, ecjia::CONFIG_CHECK)) {
-            ecjia_config::instance()->delete_config(touch::STORAGEKEY_app_description);
+//         if (ecjia::config(touch::STORAGEKEY_pc_url, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_pc_url);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_touch_url, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_touch_url);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_iphone_download, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_iphone_download);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_android_download, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_android_download);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_ipad_download, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_ipad_download);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_app_icon, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_app_icon);
+//         }
+//         if (ecjia::config(touch::STORAGEKEY_app_description, ecjia::CONFIG_CHECK)) {
+//             ecjia_config::instance()->delete_config(touch::STORAGEKEY_app_description);
+//         }
+        if (ecjia::config(touch::STORAGEKEY_map_qq_key, ecjia::CONFIG_CHECK)) {
+        	ecjia_config::instance()->delete_config(touch::STORAGEKEY_map_qq_key);
         }
 
         return true;
