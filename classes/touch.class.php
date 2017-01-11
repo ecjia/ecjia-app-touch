d922af42ea9695957308e584e7a4e03b9b23b3fc
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-11 10:50:37 +0800

    Revert "H5配置清理"
    This reverts commit 7da23c1b3ece54937dd737a0f1fbde4e7165a9b2.

--- classes/touch.class.php
+++ classes/touch.class.php
@@ -51,16 +51,16 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class touch {
     //预设配置项存储key
-//     const STORAGEKEY_template   = 'touch_template';
-//     const STORAGEKEY_stylename  = 'touch_stylename';
+    const STORAGEKEY_template   = 'touch_template';
+    const STORAGEKEY_stylename  = 'touch_stylename';
 
-//     const STORAGEKEY_pc_url             = 'shop_pc_url';
-//     const STORAGEKEY_touch_url          = 'shop_touch_url';
-//     const STORAGEKEY_iphone_download    = 'shop_iphone_download';
-//     const STORAGEKEY_android_download   = 'shop_android_download';
-//     const STORAGEKEY_ipad_download      = 'shop_ipad_download';
-//     const STORAGEKEY_app_icon           = 'shop_app_icon';
-//     const STORAGEKEY_app_description    = 'shop_app_description';
+    const STORAGEKEY_pc_url             = 'shop_pc_url';
+    const STORAGEKEY_touch_url          = 'shop_touch_url';
+    const STORAGEKEY_iphone_download    = 'shop_iphone_download';
+    const STORAGEKEY_android_download   = 'shop_android_download';
+    const STORAGEKEY_ipad_download      = 'shop_ipad_download';
+    const STORAGEKEY_app_icon           = 'shop_app_icon';
+    const STORAGEKEY_app_description    = 'shop_app_description';
     
     const STORAGEKEY_map_qq_key   		= 'map_qq_key';
 }
