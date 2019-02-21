5338a38e905fb932d593226371521d6dc1315c84
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-02-21 10:19:22 +0800

    更新应用配置文件中的语言包调用

--- languages/zh_CN/package.lang.php
+++ languages/zh_CN/package.lang.php
@@ -51,7 +51,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * @author songqian
  */
 return array(
-	'touch' 		=> 'H5应用',
+	'touch' 		=> '微商城',
 	'touch_desc'	=> '采用APP方式设计，标准通用标记语言下的一个应用超文本标记语言(HTML)重大突破。  '
 );
 
